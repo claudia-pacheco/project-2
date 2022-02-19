2fb2a59a1141214f9773598836b3457ecc001c32
Claudia <claudiaescola7@gmail.com>
2022-02-19 14:20:16 +0000

    fixed syntax

--- src/Components/Classes.js
+++ src/Components/Classes.js
@@ -1,51 +1,84 @@
 import React from "react";
+//importing images to display
+import barbarian from "../assets/barbarian.JPG";
+import bard from "../assets/bard.JPG";
+import cleric from "../assets/cleric.JPG";
+import druid from "../assets/druid.JPG";
+import fighter from "../assets/fighter.JPG";
+import monk from "../assets/monk.JPG";
+import paladin from "../assets/paladin.JPG";
+import ranger from "../assets/ranger.JPG";
+import rogue from "../assets/rogue.JPG";
+import sorcerer from "../assets/sorcerer.JPG";
+import warlock from "../assets/warlock.JPG";
+import wizard from "../assets/wizard.JPG";
 
 function ClassesDisplay() {
+  const [classDisplay, updateClassDisplay] = React.useState([]);
 
-    const [classDisplay, updateClassDisplay] = React.useState(undefined)
-
+  React.useEffect(() => {
     async function fetchClass() {
-        const resp = await fetch("https://www.dnd5eapi.co/api/classes")
-        const classDisplay = await resp.json()
-        updateClassDisplay(classDisplay)
+      const resp = await fetch("https://www.dnd5eapi.co/api/classes");
+      const classDisplay = await resp.json();
+      updateClassDisplay(classDisplay);
     }
+    fetchClass();
+  }, []);
 
-fetchClass()
-    if (classDisplay.index=== "barbarian") {
-        classImg = barbarian
-    } if (classDisplay.index=== "bard") {
-        classImg = bard
-    } if (classDisplay.index=== "cleric") {
-        classImg = cleric
-    } if (classDisplay.index=== "druid") {
-        classImg = druid
-    } if (classDisplay.index=== "fighter") {
-        classImg = fighter
-    } if (classDisplay.index=== "monk") {
-        classImg = monk
-    } if (classDisplay.index=== "paladin") {
-        classImg = paladin
-    } if (classDisplay.index=== "ranger") {
-        classImg = ranger
-    } if (classDisplay.index=== "rogue") {
-        classImg = rogue
-    } if (classDisplay.index=== "sorcerer") {
-        classImg = sorcerer
-    } if (classDisplay.index=== "warlock") {
-        classImg = warlock
-    } if (classDisplay.index=== "wizard") {
-        classImg = wizard
+  function displayImage(classDisplay) {
+    if (classDisplay.index === "barbarian") {
+      return barbarian;
     }
-    updateClassImg(classImg)
+    if (classDisplay.index === "bard") {
+      return bard;
+    }
+    if (classDisplay.index === "cleric") {
+      return cleric;
+    }
+    if (classDisplay.index === "druid") {
+      return druid;
+    }
+    if (classDisplay.index === "fighter") {
+      return fighter;
+    }
+    if (classDisplay.index === "monk") {
+      return monk;
+    }
+    if (classDisplay.index === "paladin") {
+      return paladin;
+    }
+    if (classDisplay.index === "ranger") {
+      return ranger;
+    }
+    if (classDisplay.index === "rogue") {
+      return rogue;
+    }
+    if (classDisplay.index === "sorcerer") {
+      return sorcerer;
+    }
+    if (classDisplay.index === "warlock") {
+      return warlock;
+    }
+    if (classDisplay.index === "wizard") {
+      return wizard;
+    }
+  }
+  return (
+    <>
+      <h1>Classes</h1>
 
-
-    return (
-        <>
-            <h1>Classes</h1>
-            {classDisplay.name}
-            {/* <img src={}></img> */}
-        </>
-    );
+      {classDisplay.length !== 0 ? (
+        classDisplay.results.map((elem) => (
+          <div key={elem.index}>
+            <img src={displayImage(elem)}></img>
+            <p>{elem.name}</p>
+          </div>
+        ))
+      ) : (
+        <p>... loading</p>
+      )}
+    </>
+  );
 }
 
 export default ClassesDisplay;
