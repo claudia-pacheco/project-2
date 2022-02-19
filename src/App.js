a98b7fadd5b7022d6e06d338b416890c086d50ee
Claudia <claudiaescola7@gmail.com>
2022-02-19 11:31:59 +0000

    fixed syntax

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Home from './Components/Home'
-import Races from './Components/Races'
-import Classes from './Components/Classes'
-import Nav from './Components/Nav'
+import Home from "./Components/Home"
+import Races from "./Components/Races"
+import Classes from "./Components/Classes"
+import Nav from "./Components/Nav"
 
-function App(){
+function App() {
 
-    return (
+  return (
     <Router>
       <Routes>
-          <Route path="/" element={<Nav  />} >
-            <Route index element={<Home />} />
-            <Route path="/races" element={<Races  />} />
-            <Route path="/classes" element={<Classes />} />
+        <Route path="/" element={<Nav />} >
+          <Route index element={<Home />} />
+          <Route path="/races" element={<Races />} />
+          <Route path="/classes" element={<Classes />} />
         </Route>
       </Routes>
     </Router>
