1aa4aa49a44ca63386feccf759466910c786c6ba
Claudia <claudiaescola7@gmail.com>
2022-02-19 11:40:09 +0000

    fixed syntax

--- src/Components/Classes.js
+++ src/Components/Classes.js
@@ -1,13 +1,12 @@
-import React from "react"
+import React from "react";
 
 function ClassesDisplay() {
-
-    return <>
-        <h1>Classes</h1>
-
-        {/* <img src={}></img> */}
+  return (
+    <>
+      <h1>Classes</h1>
+      {/* <img src={}></img> */}
     </>
-
+  );
 }
 
-export default ClassesDisplay
+export default ClassesDisplay;
