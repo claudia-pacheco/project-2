22bd98106f40067735ff1f9a6817ee1bed7306c5
Claudia <claudiaescola7@gmail.com>
2022-02-19 13:38:18 +0000

    fixed syntax

--- src/Components/Classes.js
+++ src/Components/Classes.js
@@ -1,12 +1,51 @@
 import React from "react";
 
 function ClassesDisplay() {
-  return (
-    <>
-      <h1>Classes</h1>
-      {/* <img src={}></img> */}
-    </>
-  );
+
+    const [classDisplay, updateClassDisplay] = React.useState(undefined)
+
+    async function fetchClass() {
+        const resp = await fetch("https://www.dnd5eapi.co/api/classes")
+        const classDisplay = await resp.json()
+        updateClassDisplay(classDisplay)
+    }
+
+fetchClass()
+    if (classDisplay.index=== "barbarian") {
+        classImg = barbarian
+    } if (classDisplay.index=== "bard") {
+        classImg = bard
+    } if (classDisplay.index=== "cleric") {
+        classImg = cleric
+    } if (classDisplay.index=== "druid") {
+        classImg = druid
+    } if (classDisplay.index=== "fighter") {
+        classImg = fighter
+    } if (classDisplay.index=== "monk") {
+        classImg = monk
+    } if (classDisplay.index=== "paladin") {
+        classImg = paladin
+    } if (classDisplay.index=== "ranger") {
+        classImg = ranger
+    } if (classDisplay.index=== "rogue") {
+        classImg = rogue
+    } if (classDisplay.index=== "sorcerer") {
+        classImg = sorcerer
+    } if (classDisplay.index=== "warlock") {
+        classImg = warlock
+    } if (classDisplay.index=== "wizard") {
+        classImg = wizard
+    }
+    updateClassImg(classImg)
+
+
+    return (
+        <>
+            <h1>Classes</h1>
+            {classDisplay.name}
+            {/* <img src={}></img> */}
+        </>
+    );
 }
 
 export default ClassesDisplay;
--- src/Components/Races.js
+++ src/Components/Races.js
@@ -2,21 +2,21 @@ import React from "react";
 import gnome from "../assets/Gnome-removebg-preview.png";
 
 function RacesDisplay() {
-  // const [race, updateRace] = React.useState(undefined)
-  // const [raceImg, updateRaceImg]
-  // async function fetchRace() {
-  //    const resp = await fetch('https://www.dnd5eapi.co/api/races/')
-  //    const race = await resp.json()
-  //    updateRace(race)
-  // }
-  // fetchRace()
+//   const [race, updateRace] = React.useState(undefined)
+// //   const [raceImg, updateRaceImg]
+//   async function fetchRace() {
+//      const resp = await fetch('https://www.dnd5eapi.co/api/races/')
+//      const race = await resp.json()
+//      updateRace(race)
+//   }
+//   fetchRace()
   return (
     <>
-      <h1>Races </h1>
-      <div>
-        <img src={gnome}></img>
-        {/* <p>{race.name}</p> */}
-      </div>
+      <h1>Races </h1><img src={gnome}></img>
+      {/* <div>
+        
+        <p>{race.name}</p>
+      </div> */}
     </>
   );
 }
